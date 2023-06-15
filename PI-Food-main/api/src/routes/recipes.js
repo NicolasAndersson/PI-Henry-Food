8e6e45a849989b93482805488117243c8988922b
Nicox <anderssonnicolas898@gmail.com>
2023-06-15 03:03:44 -0300

    BACK

--- PI-Food-main/api/src/routes/recipes.js
+++ PI-Food-main/api/src/routes/recipes.js
@@ -4,3 +4,8 @@ const {
   getDataBaseID,
   get_ApiID,
 } = require("../controllers/recipe");
+const router = Router();
+
+// GET /recipes?name="...":
+// Obtener un listado de las recetas que contengan la palabra ingresada como query parameter
+// Si no existe ninguna receta mostrar un mensaje adecuado
