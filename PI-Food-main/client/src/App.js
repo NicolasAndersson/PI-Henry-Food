a056f0b173d93e3a5350b3d3216368308a842a89
Nicox <anderssonnicolas898@gmail.com>
2023-06-23 12:00:46 -0300

    Food Src client

--- PI-Food-main/client/src/App.js
+++ PI-Food-main/client/src/App.js
@@ -1,21 +1,22 @@
-import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import HomePage from "./components/home/HomePage";
-import LandingPage from "./components/landingPage/LandingPage";
-import AddRecipe from "./components/addRecipe/AddRecipe";
-import RecipeDetails from "./components/recipeDetail/RecipeDetails";
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import LandingPage from './components/LandingPage';
+import AddRecipe from './components/AddRecipe';
+import RecipeDetails from './components/RecipeDetails';
+
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/home" exact component={HomePage} />
-          <Route path="/recipe" exact component={AddRecipe} />
-          <Route path="/home/:id" component={RecipeDetails} />
-        </Switch>
-      </div>
+    <div className="App">    
+      <Switch>
+        <Route exact path="/" component={LandingPage}/>
+        <Route path="/home" exact component={HomePage}/>
+        <Route path="/recipe" exact component={AddRecipe}/>
+        <Route path="/home/:id" component={RecipeDetails}/>        
+      </Switch>    
+    </div>
     </BrowserRouter>
   );
 }
--- PI-Food-main/client/src/store/index.js
+++ PI-Food-main/client/src/store/index.js
@@ -1,13 +1,13 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "../reducer";
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from '../reducer';
 import thunk from "redux-thunk";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk)
-    // la librería redux-devtools-extension tiene composeWithDevTools
-  ) //
-);
+const store = createStore(rootReducer,
+    compose(
+       applyMiddleware(thunk)
+         // la librería redux-devtools-extension tiene composeWithDevTools
+       )                                                                             //
+   );
 
-export default store;
+
+export default store
